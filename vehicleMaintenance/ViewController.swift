bf4086f78032a6830eeeb64d03db570d966bc23e
Andres Olarte Cuervo <aolarte9201@gmail.com>
2016-04-14 15:04:40 -0500

    creación del repositorio y pantalla inicio

---
+++ vehicleMaintenance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vehicleMaintenance
+//
+//  Created by Andres Olarte Cuervo on 4/14/16.
+//  Copyright © 2016 tallerSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
